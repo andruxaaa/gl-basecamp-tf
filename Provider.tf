c44f6e5f1deb345ca78890ea91a5e33b5c93bf27
Andrey Rososhansky <17675810+andruxaaa@users.noreply.github.com>
2020-12-23 21:00:54 +0200

    Delete Provider.tf

--- Provider.tf
+++
@@ -1,7 +0,0 @@
-# Configure the Azure provider
-provider "azurerm" {
-    version = "~>1.32.1"
-    #use_msi = true
-    #subscription_id = "xxxxxxxxxxxxxxxxx"
-    #tenant_id       = "xxxxxxxxxxxxxxxxx"
-}
